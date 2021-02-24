78654e9e8b8549f6d0677cd1aa601c0623068a4c
slk007 <shubham.soni.lko@gmail.com>
2021-02-25 00:52:41 +0530

    allowed host edit

--- RateMyMovie/settings.py
+++ RateMyMovie/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://ratemymoviedj.herokuapp.com/',]
+ALLOWED_HOSTS = ['ratemymoviedj.herokuapp.com',]
 
 
 # Application definition
